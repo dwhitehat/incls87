5be1757b834891bcaf2f9b2db9cb40e4fe20c327
dwhitehat <91549716+dwhitehat@users.noreply.github.com>
2022-06-17 13:50:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -148,3 +148,47 @@ function my_keydown(e)
      }
 }
 
+function up()
+{
+    if(player_y >= 0)
+    {
+        player_y = player_y - block_image_height;
+        console.log("block image height =" + block_image_height);
+        console.log("When Up arrow key is pressed, X = " + player_x + ",Y ="+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function down()
+{
+    if(player_y <= 500)
+    {
+        player_y = player_y + block_image_height;
+        console.log("block image height =" + block_image_height);
+        console.log("When Down arrow key is pressed, X = " + player_x + " Y ="+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function left()
+{
+    if(player_x > 0)
+    {
+        player_x = player_x - block_image_width;
+        console.log("block image width = " + block_image_width);
+        console.log("When Left arrow key is pressed, X =" + player_x + ", Y = " + player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function right()
+{
+    player_x = player_x + block_image_width;
+    console.log("block image width = " + block_image_width);
+    console.log("when reght arrow key is pressed , X = " + player_x +" Y = " + player_y);
+    canvas.remove(player_object);
+    player_update();
+}
